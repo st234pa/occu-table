a1b7430d3e0b71cb8eb1110a3cf7b52b350d2489
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-09-23 14:16:18 -0400

    fixed route

--- occu-table.py
+++ occu-table.py
@@ -2,18 +2,10 @@ from flask import Flask, render_template
 import occupations
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/occupations")
 def default():
     d = occupations.createDict()
     return render_template("occupations.html", message = occupations.select(), d=d)
-
-@app.route("/page1")
-def page1():
-    return "This is page 1."
-
-@app.route("/page2")
-def page2():
-    return "This is page 3."
 
 if __name__ == "__main__":
     app.debug = True
